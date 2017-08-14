6cfe7769589b1efc6e159689ff460f2f4245a5dd
abhi1995soni <abhishek.soni@gaurish.com>
2017-08-14 11:20:49 +0530

    Added a button.

--- Git/ViewController.swift
+++ Git/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func button(_ sender: Any) {
+   
+    print("button is pressed.")
+   
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello")
+
     }
 
     override func didReceiveMemoryWarning() {
